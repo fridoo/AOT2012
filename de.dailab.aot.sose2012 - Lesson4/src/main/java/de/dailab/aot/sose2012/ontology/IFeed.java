a8f0b46a2ecb5edd0820a8b35db6f4744088512e
Frido Dombek <fridombek@hotmail.com>
2012-06-27 16:32:32 +0200

    kleine änderungen

--- de.dailab.aot.sose2012 - Lesson4/src/main/java/de/dailab/aot/sose2012/ontology/IFeed.java
+++ de.dailab.aot.sose2012 - Lesson4/src/main/java/de/dailab/aot/sose2012/ontology/IFeed.java
@@ -10,19 +10,27 @@ public class IFeed implements IFact{
 	 */
 	private static final long serialVersionUID = 7094873408755398532L;
 	
+	private final String category;
 	private final Feed feed;
 	
+	
+
+	public String getCategory() {
+		return category;
+	}
 
 	public Feed getFeed() {
 		return feed;
 	}
 
 	public IFeed() {
+		this.category = null;
 		this.feed = null;
 	}
 	
-	public IFeed(Feed feed) {
+	public IFeed(Feed feed, String cat) {
 		this.feed = feed;
+		this.category = cat;
 	}
 
 }
